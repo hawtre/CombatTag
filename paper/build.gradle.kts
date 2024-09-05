62dcf3d13d65a5ecbc29f8500a72855f6bbb71e1
hawtre <hawtre@hawtre.net>
2024-09-05 01:59:00 +0100

    Update gradle wrapper version

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    id("io.papermc.paperweight.userdev")
-    id("xyz.jpenilla.run-paper")
+    id("io.papermc.paperweight.userdev") version "1.7.2"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 dependencies {
     paperweight {
-      paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+      paperDevBundle("1.21.1-R0.1-SNAPSHOT")
     }
 
     compileOnly("com.github.TownyAdvanced:towny:0.97.5.0")
-    compileOnly("me.confuser:BarAPI:3.5")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.11-SNAPSHOT")
 }
