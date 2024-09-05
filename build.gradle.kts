ddd56ca3034ec4bd09fc06752ad248df7757e065
hawtre <hawtre@hawtre.net>
2024-09-05 01:59:00 +0100

    Remove civgradle stuff. Not sure where it's available from. Hardcode plugin.yml name/version for now - assuming that was handling by civgradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
-import net.civmc.civgradle.CivGradleExtension
 
 plugins {
-    id("net.civmc.civgradle") version "3.1.2" apply false
 }
 
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
-    apply(plugin = "net.civmc.civgradle")
-
-    configure<CivGradleExtension> {
-        pluginName = project.property("pluginName") as String
-    }
 
     repositories {
         mavenCentral()
